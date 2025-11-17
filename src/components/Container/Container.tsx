3733ade4f81c297af936bc103d689054e73f782f
dmd-sik <apkool12@naver.com>
2025-11-17 15:24:57 +0900

    [Refactor] - Reorganize component structure according to coding rules

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React from "react";
+import styled from "styled-components/native";
 
 interface ContainerProps {
   children: React.ReactNode;
@@ -10,14 +10,14 @@ interface ContainerProps {
 const StyledContainer = styled.View<{ padding?: number; backgroundColor?: string }>`
   flex: 1;
   padding: ${(props) => props.padding || 0}px;
-  background-color: ${(props) => props.backgroundColor || '#ffffff'};
+  background-color: ${(props) => props.backgroundColor || "#ffffff"};
 `;
 
-export const Container: React.FC<ContainerProps> = ({
+export const Container = ({
   children,
   padding,
   backgroundColor,
-}) => {
+}: ContainerProps) => {
   return (
     <StyledContainer padding={padding} backgroundColor={backgroundColor}>
       {children}
