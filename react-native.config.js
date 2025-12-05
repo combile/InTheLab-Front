f48f844c01a3a1a820ecdd7e65141cf41099d70e
Minter <adf5858@naver.com>
2025-12-06 01:59:47 +0900

    feat: 비콘 플러그인 등록 및 권한 리스트 등록

--- react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dependencies: {
-    "react-native-beacons-manager": {
-      platforms: {
-        ios: null, // Disable autolinking as we use manual Podfile entry
-      },
-    },
-  },
-};
